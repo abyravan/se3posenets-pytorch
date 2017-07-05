f7f133425f9e3d7813820236f7224f4bd4667511
Franzi UW <franzi.meier@gmail.com>
2017-07-05 14:49:18 -0700

    making se3_to_rt autograd compatible

--- layers/se3_to_rt.py
+++ layers/se3_to_rt.py
@@ -1,52 +1,57 @@
 import torch
-from torch.autograd import Function
+from torch.autograd import Function, Variable
 import ipdb
 
 
-class SE3ToRtFunction(Function):
+# class SE3ToRtFunction(Function):
+#     def __init__(self, transform_type):
+#         self.output = None
+#         self.feature_size = None
+#         self.transform_type = transform_type
+
+#     def forward(self, features):
+#         self.feature_size = features.size()
+#         batch_size, numSE3s, nparams = self.feature_size
+
+#         totSE3 = batch_size * numSE3s
+#         ncols = 4
+#         output = torch.zeros((batch_size, numSE3s, 3, ncols))
+
+#         if self.transform_type == 'se3quat':
+#             rotdim = 4
+#         elif self.transform_type == 'affine':
+#             rotdim = 9
+#         else:
+#             rotdim = 3
+
+#         if self.transform_type == 'affine':
+#             output.narrow(3, 3, 1).copy_(features.narrow(2, 0, 3),
+#                                          broadcast=True)
+#             output.narrow(3, 0, 3).copy_(features.narrow(2, 3, rotdim))
+#             return output
+
+#         if self.transform_type == 'se3quat':
+#             params = features.view(totSE3, -1)
+#             quat = params.narrow(1, 3, rotdim)
+#             self.unitquat = self._computeUnitQuaternionFromQuaternion(quat)
+#             rot = output.view(totSE3, 3, ncols).narrow(2, 0, 3)
+#             rot.copy_(self._createRotFromUnitQuaternion(self.unitquat))
+
+#         transparams = params.narrow(1, 0, 3)
+#         output.narrow(3, 3, 1).copy_(transparams)
+
+#         return output
+
+#     def backward(self, grad_output):
+#         assert(self.feature_size is not None)
+
+#         # batch_size, numSE3s, nparams = self.feature_size
+#         return
+
+class SE3ToRt(torch.nn.Module):
     def __init__(self, transform_type):
-        self.output = None
-        self.feature_size = None
+        super(SE3ToRt, self).__init__()
         self.transform_type = transform_type
-
-    def forward(self, features):
-        self.feature_size = features.size()
-        batch_size, numSE3s, nparams = self.feature_size
-
-        totSE3 = batch_size * numSE3s
-        ncols = 4
-        output = torch.zeros((batch_size, numSE3s, 3, ncols))
-
-        if self.transform_type == 'se3quat':
-            rotdim = 4
-        elif self.transform_type == 'affine':
-            rotdim = 9
-        else:
-            rotdim = 3
-
-        if self.transform_type == 'affine':
-            output.narrow(3, 3, 1).copy_(features.narrow(2, 0, 3),
-                                         broadcast=True)
-            output.narrow(3, 0, 3).copy_(features.narrow(2, 3, rotdim))
-            return output
-
-        if self.transform_type == 'se3quat':
-            params = features.view(totSE3, -1)
-            quat = params.narrow(1, 3, rotdim)
-            self.unitquat = self._computeUnitQuaternionFromQuaternion(quat)
-            rot = output.view(totSE3, 3, ncols).narrow(2, 0, 3)
-            rot.copy_(self._createRotFromUnitQuaternion(self.unitquat))
-
-        transparams = params.narrow(1, 0, 3)
-        output.narrow(3, 3, 1).copy_(transparams)
-
-        return output
-
-    def backward(self, grad_output):
-        assert(self.feature_size is not None)
-
-        batch_size, numSE3s, nparams = self.feature_size
-        return
 
     def _computeUnitQuaternionFromQuaternion(self, quat):
         # N = quat.size(0)
@@ -54,19 +59,13 @@ class SE3ToRtFunction(Function):
         norm = norm2.sqrt()
 
         unitquat = torch.div(quat, norm.expand_as(quat))
-
-        # for n in range(N):
-        #     if(torch.abs(norm[n].squeeze() -1.0) <
-        #     torch.sqrt(self.threshold)):
-        #         unitquat[n].copy_(quat[n])
-        #     elif() :
-
         return unitquat
 
     def _createRotFromUnitQuaternion(self, unitquat):
 
         N = unitquat.size(0)
-        rot = torch.Tensor(N, 3, 3).type_as(unitquat)
+        rot = Variable(torch.Tensor(N, 3, 3).type_as(unitquat.data))
+
         x = unitquat.narrow(1, 0, 1)
         y = unitquat.narrow(1, 1, 1)
         z = unitquat.narrow(1, 2, 1)
@@ -93,12 +92,41 @@ class SE3ToRtFunction(Function):
         rot[:, 2, 2] = w2 - x2 - y2 + z2
         return rot
 
-
-class SE3ToRt(torch.nn.Module):
-    def __init__(self, transform_type):
-        super(SE3ToRt, self).__init__()
-        self.transform_type = transform_type
-
     def forward(self, features):
-        return SE3ToRtFunction(self.transform_type)(features)
-        # return Se3ToRtFunction(spatial_scale)(features, rois)
+        self.feature_size = features.size()
+        batch_size, numSE3s, nparams = self.feature_size
+
+        totSE3 = batch_size * numSE3s
+        ncols = 4
+
+        if self.transform_type == 'se3quat':
+            rotdim = 4
+        elif self.transform_type == 'affine':
+            rotdim = 9
+        else:
+            rotdim = 3
+
+        # if self.transform_type == 'affine':
+        #     output.narrow(3, 3, 1).copy_(features.narrow(2, 0, 3),
+        #                                  broadcast=True)
+        #     output.narrow(3, 0, 3).copy_(features.narrow(2, 3, rotdim))
+        #     return output
+
+        # output = Variable(torch.zeros((totSE3, 3, ncols)))
+        if self.transform_type == 'se3quat':
+            params = features.view(totSE3, -1)
+            quat = params.narrow(1, 3, rotdim)
+            self.unitquat = self._computeUnitQuaternionFromQuaternion(quat)
+            rotations = self._createRotFromUnitQuaternion(
+                self.unitquat)
+
+        transparams = features.view(totSE3, -1, 1).narrow(1, 0, 3)
+
+        ipdb.set_trace()
+        output = torch.cat([rotations, transparams], dim=2).view(
+            [batch_size, numSE3s, 3, ncols]).contiguous()
+
+        return output
+
+    # def forward(self, features):
+        # return SE3ToRtFunction(self.transform_type)(features)

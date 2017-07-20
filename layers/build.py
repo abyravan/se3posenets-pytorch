d1a4c3b41cccebc096a5cf3f3ce3e878edd0a2ed
Arunkumar Byravan <barun@uw.edu>
2017-07-20 14:40:51 -0700

    Updated make.sh and build.py - removed the unncessary layers

--- layers/build.py
+++ layers/build.py
@@ -3,13 +3,9 @@ import torch
 from torch.utils.ffi import create_extension
 
 # Declare CPU sources
-sources = ['src/reorg_cpu.c',
-		   'src/roi_pooling_cpu.c',
-		   'src/ntfm3d_cpu.c',
+sources = ['src/ntfm3d_cpu.c',
 		   'src/project3dpts_cpu.c']
-headers = ['src/reorg_cpu.h',
-		   'src/roi_pooling_cpu.h',
-		   'src/ntfm3d_cpu.h',
+headers = ['src/ntfm3d_cpu.h',
 		   'src/project3dpts_cpu.h']
 defines = []
 with_cuda = False
@@ -18,22 +14,16 @@ with_cuda = False
 extra_objects = None
 if torch.cuda.is_available():
     print('Including CUDA code.')
-    sources += ['src/reorg_cuda.c',
-				'src/roi_pooling_cuda.c',
-				'src/ntfm3d_cuda.c',
+    sources += ['src/ntfm3d_cuda.c',
 				'src/project3dpts_cuda.c']
-    headers += ['src/reorg_cuda.h',
-				'src/roi_pooling_cuda.h',
-				'src/ntfm3d_cuda.h',
+    headers += ['src/ntfm3d_cuda.h',
 				'src/project3dpts_cuda.h']
     defines += [('WITH_CUDA', None)]
     with_cuda = True
 
 	# Get the pre-compiled CUDA kernels (currently all CUDA code has to be compiled apriori)
     this_file = os.path.dirname(os.path.realpath(__file__))
-    extra_objects = ['src/cuda/lib/reorg_kernel.cu.o',
-					 'src/cuda/lib/roi_pooling_kernel.cu.o',
-					 'src/cuda/lib/ntfm3d_kernel.cu.o',
+    extra_objects = ['src/cuda/lib/ntfm3d_kernel.cu.o',
 					 'src/cuda/lib/project3dpts_kernel.cu.o']
     extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
 
--- make.sh
+++ make.sh
@@ -6,10 +6,8 @@ if [ -d "$CUDA_PATH" ]; then
 	cd layers/src/cuda
 	mkdir lib
 	echo "Compiling CUDA kernels using nvcc..."
-	nvcc -c -o lib/reorg_kernel.cu.o reorg_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
-	nvcc -c -o lib/roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 	nvcc -c -o lib/ntfm3d_kernel.cu.o ntfm3d_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
-    nvcc -c -o lib/project3dpts_kernel.cu.o project3dpts_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+	nvcc -c -o lib/project3dpts_kernel.cu.o project3dpts_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 	cd ../../
 	python build.py
 	cd ../

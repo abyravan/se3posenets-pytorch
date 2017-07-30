2b0b02eb474d346741bf1b40aded7af886e7bdd5
Arunkumar Byravan <barun@cs.washington.edu>
2017-07-30 13:48:22 -0700

    Using the updated data loader for testing too

--- main_ctrlnets.py
+++ main_ctrlnets.py
@@ -262,7 +262,7 @@ def main():
         print('==== Evaluating pre-trained network on test data ===')
         args.imgdisp_freq = 10 * args.disp_freq  # Tensorboard log frequency for the image data
         sampler = torch.utils.data.dataloader.SequentialSampler(test_dataset)  # Run sequentially along the test dataset
-        test_loader = DataEnumerator(torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+        test_loader = DataEnumerator(util.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
                                         num_workers=args.num_workers, sampler=sampler, pin_memory=args.use_pin_memory,
                                         collate_fn=test_dataset.collate_batch))
         iterate(test_loader, model, tblogger, len(test_loader), mode='test')
@@ -338,7 +338,7 @@ def main():
     print('==== Evaluating trained network on test data ====')
     args.imgdisp_freq = 10 * args.disp_freq # Tensorboard log frequency for the image data
     sampler = torch.utils.data.dataloader.SequentialSampler(test_dataset)  # Run sequentially along the test dataset
-    test_loader = DataEnumerator(torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+    test_loader = DataEnumerator(util.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
                                     num_workers=args.num_workers, sampler=sampler, pin_memory=args.use_pin_memory,
                                     collate_fn=test_dataset.collate_batch))
     iterate(test_loader, model, tblogger, len(test_loader), mode='test', epoch=args.epochs)

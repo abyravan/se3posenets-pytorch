7e985fbf4501346abc8fa378a5658a61357483f9
Arunkumar Byravan <barun@uw.edu>
2017-08-26 20:32:56 -0700

    Removed a timer and comment

--- main_ctrlnets.py
+++ main_ctrlnets.py
@@ -420,12 +420,9 @@ def iterate(data_loader, model, tblogger, num_iters,
         start = time.time()
 
         # Run the FWD pass through the network
-        # TODO: Choose only poses of links we move!
         if args.use_gt_masks:
-            stt = time.time()
             mask_1 = util.to_var(get_jt_masks(sample['masks'][:, 0].type(deftype), args.jt_ids), requires_grad=False)
             mask_2 = util.to_var(get_jt_masks(sample['masks'][:, 1].type(deftype), args.jt_ids), requires_grad=False)
-            print(time.time() - stt)
             pose_1, pose_2, [deltapose_t_12, pose_t_2] = model([pts_1, pts_2, ctrls_1])
         elif args.use_gt_poses:
             pose_1 = util.to_var(get_jt_poses(sample['poses'][:, 0], args.jt_ids).clone().type(deftype), requires_grad=False) # 0 is BG, so we add 1

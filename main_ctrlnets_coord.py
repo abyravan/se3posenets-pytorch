05de41e93fc94ca58ec3af9dd60eff29f035c6b5
Arunkumar Byravan <barun@cs.washington.edu>
2017-08-28 09:50:31 -0700

    Fixed bug in noise addition for weight sharpening during trainnig - previously all models were set to eval() and not reset to train().

--- main_ctrlnets_coord.py
+++ main_ctrlnets_coord.py
@@ -384,7 +384,6 @@ def iterate(data_loader, model, tblogger, num_iters,
                 enable_training(model.posemodel)        # Train pose model
                 enable_training(model.transitionmodel)  # Train transition model
                 disable_training(model.maskmodel)       # Don't train mask model
-                model.maskmodel.eval() # Set in eval mode (for BN & wt-sharpening noise)
             else:
                 iterprint = ('Iter: {}/{}, Co-ord iter: {}/{}, Step: {}/{}, Training mask model'.format(
                     i+1, num_iters, int((i-j) * (0.5/args.coord_iter))+1, ncoord, j+1, 2*args.coord_iter))
@@ -392,8 +391,6 @@ def iterate(data_loader, model, tblogger, num_iters,
                 disable_training(model.posemodel)       # Don't train pose model
                 disable_training(model.transitionmodel) # Don't train transition model
                 enable_training(model.maskmodel)        # Train mask model
-                model.posemodel.eval()        # Set in eval mode (for BN)
-                model.transitionmodel.eval()  # Set in eval mode (for BN)
 
         # ============ Load data ============#
         # Start timer
@@ -804,11 +801,13 @@ def get_jt_poses(poses, jt_ids):
 
 ### Enable training for all layers of a model
 def enable_training(model):
+    model.train() # Set in training mode
     for param in model.parameters():
         param.requires_grad = True
 
 ### Disable training for all layers of a model
 def disable_training(model):
+    model.eval() # Set in eval mode
     for param in model.parameters():
         param.requires_grad = False
 

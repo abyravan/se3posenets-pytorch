Fixed a minor bug in the visibility and pixelwise association computation - only BG points that are visible at t+1 (not occluded by another object) are labeled as visible now .Also visible BG points are associated to the same pixel now

main_se3posenets.py
@@ -982,9 +982,9 @@ def mask_consistency_loss(mask1, mask2, pixelassoc12, losstype='mse'):
     assert (losstype in ['mse', 'abs', 'kl'])
     bsz, nch, ht, wd = mask1.size()
     assert(mask1.is_same_size(mask2) and (pixelassoc12.size() == torch.Size([bsz, 1, ht, wd])))
-    mask1v, mask2v, pixelassoc12v = mask1.view(bsz, nch, ht*wd), \
-                                    mask2.view(bsz, nch, ht*wd), \
-                                    pixelassoc12.view(bsz, 1, ht*wd).long()
+    mask1v, mask2v, pixelassoc12v = mask1.contiguous().view(bsz, nch, ht*wd), \
+                                    mask2.contiguous().view(bsz, nch, ht*wd), \
+                                    pixelassoc12.contiguous().view(bsz, 1, ht*wd).long()
     assocmask = (pixelassoc12v != -1).type_as(mask1v) # Only these points need to be penalized
     pixelassoc12v[pixelassoc12v == -1] = 0 # These points index the first value (doesn't matter as mask = 0 for those pts)
     mask2v_1 = torch.gather(mask2v, 2, pixelassoc12v.expand_as(mask1v)) # Vals from tensor 2 in tensor 1, associated correctly
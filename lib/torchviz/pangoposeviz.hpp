97282d8ff6caae8c3bf231684e3a0ed9e6b7b77a
Arunkumar Byravan <barun@uw.edu>
2019-04-14 16:59:07 -0700

    Removed the control code & pangolin visualization code. Removed some test code and blocks code

--- lib/torchviz/pangoposeviz.hpp
+++
@@ -1,9 +0,0 @@
-class PangolinPoseViz
-{
-    public:
-        PangolinPoseViz();
-        ~PangolinPoseViz();
-
-        void update_viz(const float *gtpose, const float *predpose, const float *predmask,
-                        float *config, float *ptcloud);
-};

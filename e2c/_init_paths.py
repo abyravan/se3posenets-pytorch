3c1d45634aa0430d0bbc43ddabb99c3732339b89
Arunkumar Byravan <barun@uw.edu>
2019-04-14 16:50:39 -0700

    Removed unnecessary code for the public branch

--- e2c/_init_paths.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-
-# Set path to root directory so that local imports work
-add_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-if add_path not in sys.path:
-    sys.path.insert(0, add_path)
